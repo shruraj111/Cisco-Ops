4b7518f41e4393b36560af823976ea77e4a0b174
shruraj111 <163994329+shruraj111@users.noreply.github.com>
2024-09-17 16:26:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-provider "aws" {
-  region = us-east-1
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"     
+   }
 }
-
 resource "aws_s3_bucket" "example" {
   bucket = "my-github-test-bucket"
 
